80b8d04ea1956112b6af679d75c424aa361e8342
cganzon <crganzon@gmail.com>
2022-06-15 20:08:34 -0700

    create dom starter module

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const gameBoard = (() => {
   const gameBoard = [];
   return { gameBoard };
 })();
+
+const dom = (() => {
+  const board = document.querySelector(".board");
+  return { board };
+})();
