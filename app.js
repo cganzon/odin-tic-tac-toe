9e6be261a2c8dddae4ee0d96c43fb355ec69129a
cganzon <crganzon@gmail.com>
2022-06-15 20:11:51 -0700

    create game module and create functions to add a marker to the dom gameboard when a cell is clicked

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ const dom = (() => {
   const board = document.querySelector(".board");
   return { board };
 })();
+
+const game = (() => {
+  const addCellListeners = () => {
+    dom.board.addEventListener("click", (e) => markSpot(e.target));
+  };
+  const markSpot = (cell) => {
+    cell.textContent = "X";
+  };
+  return { addCellListeners };
+})();
+
+game.addCellListeners();
